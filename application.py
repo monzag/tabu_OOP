07cdc937c57d3b57fc4f3b011451ba3a6556e3c5
monzag <zaganiacz.m@gmail.com>
2017-07-28 20:46:14 +0200

    Add application

---
+++ application.py
@@ -0,0 +1,12 @@
+from view import*
+from events import *
+from controller import *
+
+
+def main():
+    controller = Controller()
+    controller.start()
+
+
+if __name__ == '__main__':
+    main()
